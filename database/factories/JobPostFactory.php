d55426f7a5f48c01b75eb9bdf3b7587b4628d36b
Abhishek Mishra <mishraavi790@gmail.com>
2024-06-15 10:27:19 +0545

    model and migration

---
+++ database/factories/JobPostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\JobPost;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use League\CommonMark\Node\Block\Paragraph;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\JobPost>
+ */
+class JobPostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle,
+            'description' => fake()->paragraphs(3, true),
+            'salary' => fake()->numberBetween(5_000, 150_000),
+            'location' => fake()->city,
+            'category' => fake()->randomElement(JobPost::$category),
+            'experience' => fake()->randomElement(JobPost::$experience),
+        ];
+    }
+}
