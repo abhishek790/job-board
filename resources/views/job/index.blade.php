566d36be0930c9d7360fc5cdd9301c904914fe26
Abhishek Mishra <mishraavi790@gmail.com>
2024-06-18 22:34:18 +0545

    job posting

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -3,12 +3,33 @@
      :links="['Jobs'=>route('jobs.index')]"/>
 
     <x-card class="mb-4 text-sm" >
-        <div class="mb-4 grid grid-cols-2 gap-4">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-        </div>
+        <form action="{{route('jobs.index')}}" method="GET" >
+            <div class="mb-4 grid grid-cols-2 gap-4">
+                <div>
+                    <div class="mb-1 font-semibold">Search</div>
+                   
+                    <x-text-input name="search" value="{{request('search')}}" placeholder="Search for any text"/>
+                </div>
+                <div>
+                    <div class="mb-1 font-semibold">Salary</div> 
+                    <div class="flex space-x-2">
+                        <x-text-input name="min_salary" value="{{request('min_salary')}}" placeholder="From"/>
+                        <x-text-input name="max_salary" value="{{request('max_salary')}}" placeholder="To"/>
+                    </div>
+                </div>
+                <div>
+                    <div class="mb-1 font-semibold">Experience</div>
+
+                    <x-radio-group name="experience" :options="\App\Models\JobPost::$experience"></x-radio-group>
+
+                </div>
+                <div>
+                    <div class="mb-1 font-semibold">Category</div>
+                    <x-radio-group name="category" :options="\App\Models\JobPost::$category"></x-radio-group>
+                </div>
+            </div>
+            <button class="w-full">Filter</button>
+        </form>
     </x-card>
 
     @foreach($jobs as $job)
