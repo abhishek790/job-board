91549500c774d309f7c9b50704b8ad09618d8096
Abhishek Mishra <mishraavi790@gmail.com>
2024-08-21 14:13:46 +0545

    applying jobs features

--- app/View/Components/RadioGroup.php
+++ app/View/Components/RadioGroup.php
@@ -20,7 +20,7 @@ class RadioGroup extends Component
     // defining a method to manipulate array sent to the component class
     // this method can be used just as properties in blade
     public function optionsWithLabels(): array
-    { //this method below checks if the array is associative array or not
+    { //this method below checks if the array is list  or not, an array is considered if it is not assoc array
         return array_is_list($this->options) ?
             // takes in 2 array as paramater and combines them into assco array 1st will be key and 2nd will be array 
             array_combine($this->options, $this->options) : $this->options;
